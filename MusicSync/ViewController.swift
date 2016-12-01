ada3d5c46e1297412ccedf10402b074295394865
ZiadAli <ziadali147@gmail.com>
2016-11-30 19:58:47 -0500

    Initial Commit

---
+++ MusicSync/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicSync
+//
+//  Created by Ziad Ali on 11/30/16.
+//  Copyright © 2016 ZiadCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
